aa28e34c251bee63e4f864ed251c3bf61525c1c9
29584 <2334949001@qq.com>
2025-11-11 20:54:07 +0800

    解决银行问题

--- app/src/main/java/com/xposed/briaccessibilityservice/server/PayAccessibilityService.java
+++ app/src/main/java/com/xposed/briaccessibilityservice/server/PayAccessibilityService.java
@@ -109,8 +109,8 @@ public class PayAccessibilityService extends AccessibilityService {
         }
 
         //输入银行编码
-        if (viewIdResourceMap.containsKey("id.co.bri.brimo:id/2131366508")) {
-            AccessibilityNodeInfo input = viewIdResourceMap.get("id.co.bri.brimo:id/2131366508");
+        if (viewIdResourceMap.containsKey("id.co.bri.brimo:id/2131366522")) {
+            AccessibilityNodeInfo input = viewIdResourceMap.get("id.co.bri.brimo:id/2131366522");
             AccessibleUtil.inputTextByAccessibility(input, takeLatestOrderBean.getBankName());
         }
 
