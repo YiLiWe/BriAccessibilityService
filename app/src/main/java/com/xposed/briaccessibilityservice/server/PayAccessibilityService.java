f030ee7971b3f37b7bff045ca387d66eed07dddd
29584 <2334949001@qq.com>
2025-11-11 22:52:22 +0800

    解决银行问d

--- app/src/main/java/com/xposed/briaccessibilityservice/server/PayAccessibilityService.java
+++ app/src/main/java/com/xposed/briaccessibilityservice/server/PayAccessibilityService.java
@@ -275,6 +275,7 @@ public class PayAccessibilityService extends AccessibilityService {
     @Override
     public void onAccessibilityEvent(AccessibilityEvent accessibilityEvent) {
         AccessibilityNodeInfo accessibilityNodeInfo = getRootInActiveWindow();
+        if (accessibilityNodeInfo == null) return;
         List<AccessibilityNodeInfo> errors = accessibilityNodeInfo.findAccessibilityNodeInfosByViewId("id.co.bri.brimo:id/2131366594");
         if (!errors.isEmpty()) {
             for (AccessibilityNodeInfo error : errors) {
