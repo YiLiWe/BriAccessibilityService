800beb78c5537a206c46417e829d5b2053dbd06c
29584 <2334949001@qq.com>
2025-11-12 15:05:25 +0800

    解决提交问题

--- app/src/main/java/com/xposed/briaccessibilityservice/runnable/BillRunnable.java
+++ app/src/main/java/com/xposed/briaccessibilityservice/runnable/BillRunnable.java
@@ -52,6 +52,7 @@ public class BillRunnable implements Runnable {
                 .url(text)
                 .build();
         try (Response response = okHttpClient.newCall(request).execute()) {
+            Logs.d("响应码:"+response.code());
             if (response.isSuccessful()) {
                 billDao.updateStateById(billEntity.getUid(), 1);
             }
