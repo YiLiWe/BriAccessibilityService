b8d934979cb56e6ba7c14667e1b65d3834f71ebe
29584 <2334949001@qq.com>
2025-11-12 22:33:52 +0800

    账单提交

--- app/src/main/java/com/xposed/briaccessibilityservice/server/PayAccessibilityService.java
+++ app/src/main/java/com/xposed/briaccessibilityservice/server/PayAccessibilityService.java
@@ -434,21 +434,23 @@ public class PayAccessibilityService extends AccessibilityService {
 
         if (viewIdResourceMap.containsKey("id.co.bri.brimo:id/2131368710")) {
             AccessibilityNodeInfo nodeInfo1 = viewIdResourceMap.get("id.co.bri.brimo:id/2131368710");
-            String text = nodeInfo1.getText().toString();
-            if (text.equals("Transaksi Berhasil")) {
-                if (takeLatestOrderBean != null) {
-                    success(takeLatestOrderBean);
-                } else if (collectBillResponse != null) {
-                    success(collectBillResponse);
+            if (nodeInfo1 != null) {
+                String text = nodeInfo1.getText().toString();
+                if (text.equals("Transaksi Berhasil")) {
+                    if (takeLatestOrderBean != null) {
+                        success(takeLatestOrderBean);
+                    } else if (collectBillResponse != null) {
+                        success(collectBillResponse);
+                    }
+                } else {
+                    if (takeLatestOrderBean != null) {
+                        error(text, takeLatestOrderBean);
+                    } else if (collectBillResponse != null) {
+                        error(text, collectBillResponse);
+                    }
                 }
-            } else {
-                if (takeLatestOrderBean != null) {
-                    error(text, takeLatestOrderBean);
-                } else if (collectBillResponse != null) {
-                    error(text, collectBillResponse);
-                }
+                clickButton(viewIdResourceMap, "id.co.bri.brimo:id/2131362212");
             }
-            clickButton(viewIdResourceMap, "id.co.bri.brimo:id/2131362212");
         }
     }
 
